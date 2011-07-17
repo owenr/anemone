19a2d10eb3739da1c9ad8c4de4616ff2ec828825
Owen Rodda <owen.rodda@me.com>
2011-07-17 19:53:48 +0800

    Use URI methods to strip fragments
    Replaces complex and error-prone escaping code with simpler approach

--- lib/anemone/page.rb
+++ lib/anemone/page.rb
@@ -133,16 +133,14 @@ module Anemone
 
     #
     # Converts relative URL *link* into an absolute URL based on the
-    # location of the page
+    # location of the page.  Strip any anchors (fragments).
     #
     def to_absolute(link)
       return nil if link.nil?
 
-      # remove anchor
-      link = URI.encode(URI.decode(link.to_s.gsub(/#[a-zA-Z0-9_-]*$/,'')))
-
       relative = URI(link)
       absolute = @url.merge(relative)
+      absolute.fragment = nil
 
       absolute.path = '/' if absolute.path.empty?
 
